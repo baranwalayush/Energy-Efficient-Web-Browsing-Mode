03482696d26bfdc9ba58a499fb4a651dbb666fcc
baranwalayush <ayush6262kr@gmail.com>
2024-11-14 14:15:54 +0530

    Add popup.js

---
+++ popup.js
@@ -0,0 +1,7 @@
+// Initialize buttons
+const tabManagementButton = document.getElementById("tabToggle");
+const mediaBlockingButton = document.getElementById("mediaToggle");
+const darkModeButton = document.getElementById("darkModeToggle");
+const batterySaverButton = document.getElementById("batterySaverToggle");
+const dataSaverButton = document.getElementById("dataSaverToggle");
+const dashboardButton = document.getElementById("viewDashboard");
