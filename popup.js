487300730bc6007b1d1efb0de7b4fbdb7f3f65b9
baranwalayush <ayush6262kr@gmail.com>
2024-11-14 14:37:21 +0530

    Changed normal button to toggle button

--- popup.js
+++ popup.js
@@ -5,3 +5,16 @@ const darkModeButton = document.getElementById("darkModeToggle");
 const batterySaverButton = document.getElementById("batterySaverToggle");
 const dataSaverButton = document.getElementById("dataSaverToggle");
 const dashboardButton = document.getElementById("viewDashboard");
+
+let tabManagementEnabled = false;
+
+tabManagementButton.addEventListener("click", () => {
+    tabManagementEnabled = !tabManagementEnabled;
+    //tabManagementButton.innerText = tabManagementEnabled ? "Disable" : "Enable";
+    if(tabManagementEnabled){
+        console.log("Tab Management Enabled");
+    }
+    else{
+        console.log("Tab Management Disabled");
+    }
+});
