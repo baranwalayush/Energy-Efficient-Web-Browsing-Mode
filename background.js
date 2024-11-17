1cda77ef36878609d385ab89e126f307d1dbb1bc
baranwalayush <ayush6262kr@gmail.com>
2024-11-17 13:02:39 +0530

    Add darkMode functionality

--- background.js
+++ background.js
@@ -2,77 +2,98 @@
 
 // Listen for messages from popup.js to enable/disable each feature
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action == "toggleFeature") {
+    if (request.action === "toggleFeature") {
         switch (request.feature) {
-            case "tabManagemet":
+            case "tabManagement":
                 toggleTabManagement(request.enabled);
                 break;
-            case "mediaBlocking": {
+            case "mediaBlocking":
                 toggleMediaBlocking(request.enabled);
                 break;
-            }
-            case "darkMode": {
+            case "darkMode":
                 toggleDarkMode(request.enabled);
                 break;
-            }
-            case "batterySaver": {
+            case "batterySaver":
                 toggleBatterySaver(request.enabled);
                 break;
-            }
-            case "dataSaver": {
+            case "dataSaver":
                 toggleDataSaver(request.enabled);
-            }
+                break;
         }
     }
-})
+  });
 
 
 function toggleTabManagement(enable) {
     if (enable) {
-
+        console.log("-");
     }
     else {
-
+        console.log("-");
     }
 }
 
 
 function toggleMediaBlocking(enable) {
     if (enable) {
-
+        console.log("-");
     }
     else {
-        
+        console.log("-");
     }
 }
 
 
 function toggleDarkMode(enable) {
-    if (enable) {
-        console.log("Dark Mode Enabled --- checking background.js working");
-    }
+    chrome.tabs.query({}, (tabs) => {
+        tabs.forEach(tab => {
+            chrome.scripting.executeScript({
+                target : {tabId : tab.id},
+                function : enableDarkMode,
+                args : [enable]
+            })
+            .then(() => console.log("task completed"));
+        });
+    })
+}
+
+// function isDarkMode() {
+//     const darkModeClasses = ['dark-mode', 'dark-theme', 'dark'];
+  
+//     for (const className of darkModeClasses) {
+//       if (document.body.classList.contains(className)) {
+//         return true;
+//       }
+//     }
+//     return false;
+//   }
+
+function enableDarkMode(enabled) {
+    if (enabled) {
+        document.body.style.filter = "invert(1) hue-rotate(180deg)";
+    } 
     else {
-        
+        document.body.style.filter = "";
     }
 }
 
 
 function toggleBatterySaver(enable) {
     if (enable) {
-
+        console.log("-");
     }
     else {
-        
+        console.log("-");
     }
 }
 
 
 function toggleDataSaver(enable) {
     if (enable) {
-
+        console.log("-");
     }
     else {
-        
+        console.log("-");
     }
 }
 
